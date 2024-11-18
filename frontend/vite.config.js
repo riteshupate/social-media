6ef3817f5b6036e019a43ad2ed8ce61a8437e184
riteshupate <156455567+riteshupate@users.noreply.github.com>
2024-11-18 19:29:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://social-media-l16m.onrender.com/",
 				changeOrigin: true,
 			},
 		},
