d7fbf4c72ef525e9d145c833912278226eeb545d
riteshupate <156455567+riteshupate@users.noreply.github.com>
2024-11-18 20:04:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const __dirname = path.resolve();
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: "https://peaceful-fox-18d276.netlify.app/", credentials: true }));
+app.use(cors({ origin: "https://673b503d9722921b92c86cd6--gentle-valkyrie-8d61e9.netlify.app/", credentials: true }));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
